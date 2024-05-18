88dd9de012b84cac2fe57e995f26eb716423558b
NoAngel <155915830+Angel45003@users.noreply.github.com>
2024-05-17 20:37:21 -0400

    Update BentoGrid.jsx

--- src/components/BentoGrid.jsx
+++ src/components/BentoGrid.jsx
@@ -1,3 +1,6 @@
+import Image1 from "../assets/b1.svg";
+import Image4 from "../assets/b4.svg";
+import Image5 from "../assets/b5.svg";
 import React from "react";
 import CardBento from "./ui/CardBento";
 
@@ -7,15 +10,15 @@ function BentoGrid() {
       <div className="grid md:grid-cols-2 grid-cols-1 md:w-[900px] w-[100%] mx-auto mt-[60px] text-center gap-16">
         <CardBento
           Description="Creación de Apps totalmente responsivas."
-          ImageBento="/src/assets/b1.svg"
+          ImageBento={Image1}
         />
         <CardBento
           Description="Manejos de Control en Proyectos (Dashboard)"
-          ImageBento="/src/assets/b4.svg"
+          ImageBento={Image4}
         />
         <CardBento
           Description="Optimización en Apps."
-          ImageBento="/src/assets/b5.svg"
+          ImageBento={Image5}
           IsLarge="true"
         />
       </div>
